a5c0729d1f9f83021bdf8fcef17167051207dbd8
minecode0606 <minseo_0606@naver.com>
2021-01-16 14:32:08 +0900

    2069, 1009

--- 1009.py
+++ 1009.py
@@ -1,8 +1,8 @@
 import sys
-input01 = int(input())
+input01 = int(input()) # 테스트 케이스 입력
 
 
-for _ in range(input01):
+for _ in range(input01): # 반복문 돌리기
     inputList1 = list(map(int, list(sys.stdin.readline().split())))
     final = inputList1[0] ** inputList1[1]
     devide = final % 10
