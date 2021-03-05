c13e51087f7a06cb82a0e3578cd3c6d6627f9887
minseo <minseo_0606@naver.com>
2021-03-05 22:51:51 +0900

    오답

--- 2750.py
+++ 2750.py
@@ -1,30 +1,15 @@
-# def sort_ascending(input_list):
-#     outputlist = []
-#
-#     for _ in input_list:
-#         maximum = input_list[0]
+loopinput = int(input(""))
 
+inputlist = []
+outputlist = []
+for _ in range(loopinput):
+    inputlist.append(int(input("")))
 
-# if __name__ == "__main__":
-#     ln = []
-#     forloop_input = int(input(""))
-#     for _ in range(forloop_input):
-#         inputappend = int(input(""))
-#         ln.append(inputappend)
-#     output = sort_ascending(ln)
-#     print(output)
-
-# loop_input = int(input(""))
-# inputlist = []
-# outputlist = []
-# for _ in range(loop_input):
-#     forinput = int(input(""))
-#     inputlist.append(forinput)
-
-# for _ in inputlist:
-#     maximimum = inputlist[0]
-#     for j in inputlist:
-#         if maximimum <= j:
-#             outputlist.append(j)
-#     inputlist.pop(0)
-# print(outputlist)
+for _ in range(len(inputlist)):
+    for i in inputlist:
+        maximum = inputlist[0]
+        if maximum < i:
+            maximum = i
+    outputlist.append(maximum)
+    inputlist.remove(maximum)
+print(outputlist)
