f3df499ae8dd6acdfdbab98194d47c2f4c89c1ef
minseo <minseo_0606@naver.com>
2021-03-06 22:43:40 +0900

    오답

--- 14696.py
+++ 14696.py
@@ -1,25 +1,24 @@
 import sys
 
 loopinput = int(input(""))
+select_index_list = [4, 3, 2, 1]
 
-comparecountA = 0
-comparecountB = 0
-forlooplist = [4, 3, 2, 1]
 for _ in range(loopinput):
-    playerA_card_input = list(map(int, list(sys.stdin.readline().split())))
-    playerB_card_input = list(map(int, list(sys.stdin.readline().split())))
-
-    for index in forlooplist:
-        if index in playerA_card_input or index in playerB_card_input:
-            for playerA_star in playerA_card_input:
-                if playerA_star == index:
-                    comparecountA += 1
-            for playerB_star in playerB_card_input:
-                if playerB_star == index:
-                    comparecountB += 1
-        if comparecountA == comparecountB:
-            print("D")
-        elif comparecountA > comparecountB:
-            print("A")
-        else:
-            print("B")
+    playerA_input_list = list(map(int, list(sys.stdin.readline().split())))
+    playerB_input_list = list(map(int, list(sys.stdin.readline().split())))
+    output = 0
+    for index in select_index_list:
+        compareA = 0
+        compareB = 0
+        if index in playerA_input_list:
+            compareA += 1
+        if index in playerB_input_list:
+            compareB += 1
+        if compareA > 0 or compareB > 0:
+            if compareA > compareB:
+                print("A")
+            elif compareA < compareB:
+                print("B")
+            else:
+                print("D")
+            break
