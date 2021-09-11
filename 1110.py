afddc77744ca5de55b7f182b959474d5866035fa
Minseokang <minseo_0606@naver.com>
2021-09-11 15:51:15 +0900

    오답

--- 1110.py
+++ 1110.py
@@ -9,16 +9,22 @@ def input_preprocessing(inputdata):
     return inputdata
 
 
-def add_cycling(input_string):
-    count = 0
-    update_num = str(int(input_string[0]) + int(input_string[1]))
-    cal_num = update_num
-
-    while str(update_num) == str(input_string):
-        pass
+# def add_cycling(input_string):
+#     count = 0
+#     update_num = str(int(input_string[0]) + int(input_string[1]))
+#     cal_num = update_num
+#
+#     while str(update_num) == str(input_string):
+#         pass
 
 
 if __name__ == '__main__':
+    # 정수 N을 입력받습니다.
     input_str = input_preprocessing(input(""))
 
-
+    count = 0 # 사이클 수를 세는 변수입니다.
+    result = input_str
+    inputls = []
+    while int(input_str) == int(result):
+        for i in input_str:
+            pass
