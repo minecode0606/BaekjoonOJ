206eaabdeaefa5669ec963967868b2e67602a902
Minseokang <minseo_0606@naver.com>
2021-09-11 15:47:41 +0900

    정답

--- 1009.py
+++ 1009.py
@@ -1,8 +1,12 @@
 import sys
-loopinput = int(input(""))
-
-for _ in range(loopinput):
+testcase = int(sys.stdin.readline())
+for _ in range(testcase):
     inputlist = list(map(int, list(sys.stdin.readline().split())))
-    dataset = inputlist[0] ** inputlist[1]
-    computerindex = dataset % 10
-    print(computerindex)
+    base, up = inputlist[0] , inputlist[1]
+    if up % 4 == 0:
+        up = 4
+    else:
+        up = up % 4
+    dataset = base ** up % 10
+    # computerindex = dataset % 10
+    print(dataset)
