84951a9806bfc4caeb5427daef8ca021458e9fef
Minseokang <minseo_0606@naver.com>
2021-09-12 11:41:35 +0900

    정답

--- 3052.py
+++ 3052.py
@@ -1,26 +1,16 @@
-def remainder(input01, input02, input03, input04, input05,
-              input06, input07, input08, input09, input10):
+def input_elenemt():
+    outls = []
+    for _ in range(10):
+        inputelemint = int(input(""))
+        outls.append(inputelemint)
+    return outls
 
-    input01 = input01 % 42
-    input02 = input02 % 42
-    input03 = input03 % 42
-    input04 = input04 % 42
-    input05 = input05 % 42
-    input06 = input06 % 42
-    input07 = input07 % 42
-    input08 = input08 % 42
-    input09 = input09 % 42
-    input10 = input10 % 42
-
-    inputarray = [input01, input02, input03, input04, input05,
-              input06, input07, input08, input09, input10]
-    ls = []
-    for i in inputarray:
-        if i not in inputarray:
-            ls.append(i)
-
-    return ls
+def compare_and_devices(input_ls : list):
+    remainder_ls = []
+    for i in input_ls:
+        remainder_ls.append(i % 42)
+    return len(set(remainder_ls))
 
 if __name__ == '__main__':
-    input01, input02, input03, input04, input05, input06, input07, input08, input09, input10 = int(input("")), int(input("")), int(input("")), int(input("")), int(input("")), int(input("")), int(input("")), int(input("")), int(input("")), int(input(""))
-    print(remainder(input01, input02, input03, input04, input05, input06, input07, input08, input09, input10))
+    inputls = input_elenemt()
+    print(compare_and_devices(inputls))
